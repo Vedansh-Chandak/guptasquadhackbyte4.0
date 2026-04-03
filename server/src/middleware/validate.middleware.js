994065b8d44ecb530c91dca8d2c66f075ce16142
Hackersbot2003 <482003vinaypatidar@gmail.com>
2026-04-04 04:41:42 +0530

    validation middleware

--- server/src/middleware/validate.middleware.js
+++ server/src/middleware/validate.middleware.js
@@ -1,1 +1,23 @@
-//validate
+/**
+ * Validation middleware
+ * Handles validation errors from express-validator
+ */
+
+const { validationResult } = require("express-validator");
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      message: "Validation failed",
+      errors: errors.array().map((err) => ({
+        field: err.param,
+        message: err.msg,
+      })),
+    });
+  }
+  next();
+};
+
+module.exports = validate;
