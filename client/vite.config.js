e896512c93e767b4a4a05e77987d8aeac3af2f75
Hackersbot2003 <482003vinaypatidar@gmail.com>
2026-04-04 19:05:53 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0',
-    https: true,
-    hmr: {
-      host: '0.0.0.0',
-      protocol: 'wss',
-    },
+    host: '127.0.0.1',  // ← add this line
   }
 })
